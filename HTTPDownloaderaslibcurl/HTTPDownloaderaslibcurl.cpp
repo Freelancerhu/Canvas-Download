68572df9f1d5b499228010d4c364c89a580ee2c4
JHU24 <qq225278441@gmail.com>
2016-11-10 22:52:37 -0800

    Update HTTPDownloaderaslibcurl.cpp

--- HTTPDownloaderaslibcurl/HTTPDownloaderaslibcurl.cpp
+++ HTTPDownloaderaslibcurl/HTTPDownloaderaslibcurl.cpp
@@ -22,16 +22,17 @@ HTTPDownloader::~HTTPDownloader() {
 
 string HTTPDownloader::download(const std::string& url) {
     curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-    /* example.com is redirected, so we tell libcurl to follow redirection */
+    // example.com is redirected, so we tell libcurl to follow redirection 
     curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
-    curl_easy_setopt(curl, CURLOPT_NOSIGNAL, 1); //Prevent "longjmp causes uninitialized stack frame" bug
+    //Prevent "longjmp causes uninitialized stack frame" bug
+    curl_easy_setopt(curl, CURLOPT_NOSIGNAL, 1); 
     curl_easy_setopt(curl, CURLOPT_ACCEPT_ENCODING, "deflate");
     std::stringstream out;
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, &out);
-    /* Perform the request, res will get the return code */
+    // Perform the request, res will get the return code 
     CURLcode res = curl_easy_perform(curl);
-    /* Check for errors */
+    // Check for errors 
     if (res != CURLE_OK) {
         fprintf(stderr, "curl_easy_perform() failed: %s\n",
                 curl_easy_strerror(res));
