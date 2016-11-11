d869ff92a4314701e4846380de140a84365d1ea8
lkyllx0 <mhe6abc@gmail.com>
2016-11-11 01:20:37 -0500

    cUrl

---
+++ Libcurl connection to Canvas/HTTPDownloader.h
@@ -0,0 +1,15 @@
+#ifndef HTTPDOWNLOADER_HPP
+#define HTTPDOWNLOADER_HPP
+
+#include <string>
+
+class HTTPDownloader {
+public:
+    HTTPDownloader();
+    ~HTTPDownloader();
+    std::string download(const std::string& url);
+private:
+    void* curl;
+};
+
+#endif
